858985c7738d41e1ba51f63615cb6c86449451ad
LRTT <62591302+LRTT@users.noreply.github.com>
2020-04-28 07:14:13 +0700

    Delete example_callback.py

--- example_callback.py
+++
@@ -1,8 +0,0 @@
-from newqr import NewQRLogin
-from linepy import LINE
-
-def sendToChat(client, to, header):
-    newqr = NewQRLogin(lambda output: client.sendMessage(to, output))
-    method = newqr.loginQRWithWebPinCode
-    token, cert = newqr.parseLogin(method(header))
-    client.sendMessage(to, "TOKEN: %s\nCERT: %s" % (token, cert))
