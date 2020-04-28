03b2c218bc2afa409c046d0c643f72f3aed90e90
LRTT <62591302+LRTT@users.noreply.github.com>
2020-04-28 07:14:04 +0700

    Update newqr.py

--- newqr.py
+++ newqr.py
@@ -1,78 +1,27 @@
 import requests
+import urllib.parse
 
 class NewQRLogin:
-    HEADERS = ["android_lite", "android", "ios_ipad", "ios", "chrome", "desktopwin", "desktopmac"]
-    BASE_HOST = "https://api.lrtt.icu/secondaryQrCodeLogin.do/"
+    API_URL = "http://127.0.0.1:8000/secondaryQrCodeLogin.do"
 
-    def __init__(this, callback=lambda x: print(x)):
-        this.callback = callback
+    def loginWithQrCode(self, header, certificate="", callback=lambda output: print(output)):
+        resp = requests.post(self.API_URL + "/login?" + urllib.parse.urlencode({"header": header, "certificate": certificate}))
+        res = resp.json()
+        if resp.status_code != 200:
+            raise Exception(res)
+        callback("Login URL: %s" % (res["url"]))
 
-    def get(this, URL):
-        return requests.get(URL).json()
+        while "token" not in res:
+            resp = requests.post(self.API_URL + res["callback"])
+            res = resp.json()
+            if resp.status_code != 200:
+                raise Exception(res)
 
-    def getHeaders(this, URL):
-        return requests.get(URL).json()
+            if "pin" in res:
+                callback("Input PIN: %s" % (res["pin"]))
 
-    def parseLogin(this, loginInfo):
-        return (loginInfo["token"], loginInfo["certificate"])
+        return res
 
-    def loginQR(this, header):
-        assert header in this.HEADERS
-        result = this.get(this.BASE_HOST + "login?headers=" + header)
-        if result["status"] != 200:
-            raise Exception(result["reason"])
-        this.callback("Login Url: %s" % (result["url"]))
-        result = this.get(this.BASE_HOST + result["callback"])
-        if result["status"] != 200:
-            raise Exception(result["reason"])
-        this.callback("Pin Code: %s" % (result["pincode"]))
-        result = this.get(this.BASE_HOST + result["callback"])
-        if result["status"] != 200:
-            raise Exception(result["reason"])
-        return result
-
-    def loginQRWithWebPinCode(this, header, lang="en"):
-        # SUPPORT LANGUAGE: [en, th, jp]
-        assert header in this.HEADERS
-        result = this.get(this.BASE_HOST + "login?headers=" + header)
-        if result["status"] != 200:
-            raise Exception(result["reason"])
-        this.callback(
-            "Pin Url: %sawaitPinCode?session=%s&lang=%s" % (this.BASE_HOST, result["session"], lang) + "\n"
-            "Login Url: %s" % (result["url"])
-        )
-        result = this.get(this.BASE_HOST + result["callback"])
-        if result["status"] != 200:
-            raise Exception(result["reason"])
-        result = this.get(this.BASE_HOST + result["callback"])
-        if result["status"] != 200:
-            raise Exception(result["reason"])
-        return result
-
-    def loginQRWithCert(this, header, certificate):
-        assert header in this.HEADERS
-        result = this.get(this.BASE_HOST + "login?headers=" + header + "&certificate=" + certificate)
-        if result["status"] != 200:
-            raise Exception(result["reason"])
-        this.callback("Login Url: %s" % (result["url"]))
-        result = this.get(this.BASE_HOST + result["callback"])
-        if result["status"] != 200:
-            raise Exception(result["reason"])
-        return result
-
-    def loginQRWithCertV2(this, header, certificate=""):
-        assert header in this.HEADERS
-        result = this.get(this.BASE_HOST + "login?headers=" + header + "&certificate=" + certificate + "&type=2")
-        if result["status"] != 200:
-            raise Exception(result["reason"])
-        this.callback("Login Url: %s" % (result["url"]))
-        result = this.get(this.BASE_HOST + result["callback"])
-        if result["status"] == 500:
-            raise Exception(result["reason"])
-        if result["status"] != 409:
-            return result
-        this.callback("Pin Code: %s" % (result["pincode"]))
-        result = this.get(this.BASE_HOST + result["callback"])
-        if result["status"] != 200:
-            raise Exception(result["reason"])
-        return result
+if __name__ == "__main__":
+    qrv2 = QrV2()
+    qrv2.loginWithQrCode("android_lite")
