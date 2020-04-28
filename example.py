446874e3f20c8831d88368dbb5631566677f0e9b
LRTT <62591302+LRTT@users.noreply.github.com>
2020-04-28 07:14:26 +0700

    Update example.py

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ newqr = NewQRLogin()
 print("Headers: %s" % (", ".join(newqr.HEADERS)))
 header = input("Header: ")
 
-method = newqr.loginQRWithWebPinCode
+method = newqr.loginWithQrCode
 token, cert = newqr.parseLogin(method(header))
 
 client = LINE(token)
